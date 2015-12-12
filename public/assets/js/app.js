21103f462fcb70b8db615b4fd56d2786d9a4694a
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2015-12-12 16:10:23 +0000

    Add Angular files.

---
+++ public/assets/js/app.js
@@ -0,0 +1,12 @@
+use 'strict';
+
+var mimosApp = angular.module('mimosApp', [
+  'ngRoute',
+  'mimosController',
+  'mimosServices'
+]);
+
+mimosApp.config(['$routeProvider',
+  function($routeProvider) {
+
+  }]);
