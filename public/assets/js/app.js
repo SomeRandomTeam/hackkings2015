991276b7ef64f0ce55c653a74041c2b3e860e487
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2015-12-12 21:58:00 +0000

    Update User Profile

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,3 +14,16 @@ mymosApp.controller('UserController', function($scope, $http) {
     $http.get("http://www.w3schools.com/angular/customers.php")
     .then(function(response) {$scope.users = response.data.records;});
 });
+
+mymosApp.controller('ProfileController', function($scope, $http) {
+    $http.get("/api/getmyself")
+    .then(function(r) {
+      return $http.get("/api/users/" + r._id);
+    }).then(function(t) {
+      $scope.$apply(function() {
+        $scope.prof = t;
+      })
+
+    });
+
+});
