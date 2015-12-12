dee513e0625210cd4f55c1b2cfa32e991f04d7bd
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2015-12-12 21:20:37 +0000

    Use angular ng-repeat for user list

---
+++ public/assets/mData/user.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+  { "records": [
+  {
+      "name": "user1",
+      "id": "u1",
+      "img": "/assets/img/img1.jpg"
+  },
+  {
+      "name": "user2",
+      "id": "u2",
+      "img": "/assets/img/img1.jpg"
+  },
+  {
+      "name": "user3",
+      "id": "u3",
+      "img": "/assets/img/img1.jpg"
+  },
+  {
+      "name": "user4",
+      "id": "u4",
+      "img": "/assets/img/img1.jpg"
+  }
+]
+}
+</body>
+</html>
